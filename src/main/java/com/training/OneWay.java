118a7ac8e63334c59d0459807d4b4be08b5b0dd4
Nachiket Doke <ndoke@cisco.com>
2019-08-30 14:48:05 +0530

    Adds code to check if two strings are one edit away.

--- src/main/java/com/training/OneWay.java
+++ src/main/java/com/training/OneWay.java
@@ -2,6 +2,10 @@ package com.training;
 
 public class OneWay {
     public boolean isOneEditAwaySeparate(String input1, String input2) {
+        if (isDiffMoreThanTwo(input1, input2)) {
+            return false;
+        }
+
         if (input1.length() == input2.length()) {
             return checkIfOneEditAwayEqual(input1, input2);
         } else {
@@ -49,11 +53,41 @@ public class OneWay {
     }
 
     public boolean isOneEditAwayCombined(String input1, String input2) {
-        return checkIfOneEditAway(input1, input2);
+        if (isDiffMoreThanTwo(input1, input2)) {
+            return false;
+        }
+
+        if (input1.length() > input2.length()) {
+            return checkIfOneEditAway(input1, input2);
+        }
+        return checkIfOneEditAway(input2, input1);
     }
 
-    private boolean checkIfOneEditAway(String input1, String input2) {
+    private boolean checkIfOneEditAway(String longer, String shorter) {
+        int i = 0, j = 0;
+        boolean diffFound = false;
 
+        while (i < shorter.length() && j < longer.length()) {
+            if (longer.charAt(j) != shorter.charAt(i)) {
+                if (diffFound) {
+                    return false;
+                }
+                diffFound = true;
+                if (longer.length() > shorter.length()) {
+                    j++;
+                }
+            }
+            i++;
+            j++;
+        }
+
+        if (!diffFound && longer.length() == shorter.length()) {
+            return false;
+        }
         return true;
+    }
+
+    private boolean isDiffMoreThanTwo(String input1, String input2) {
+        return Math.abs(input1.length() - input2.length()) > 1;
     }
 }
--- src/test/java/com/training/OneWayTest.java
+++ src/test/java/com/training/OneWayTest.java
@@ -10,9 +10,18 @@ public class OneWayTest {
     public void test() {
         Assert.assertTrue(oneWay.isOneEditAwaySeparate("pale", "ple"));
         Assert.assertTrue(oneWay.isOneEditAwaySeparate("pales", "pale"));
+        Assert.assertFalse(oneWay.isOneEditAwaySeparate("palesx", "pale"));
         Assert.assertTrue(oneWay.isOneEditAwaySeparate("bale", "pale"));
         Assert.assertFalse(oneWay.isOneEditAwaySeparate("pale", "bae"));
         Assert.assertFalse(oneWay.isOneEditAwaySeparate("geeks", "geeks"));
         Assert.assertFalse(oneWay.isOneEditAwaySeparate("peaks", "geeks"));
+
+        Assert.assertTrue(oneWay.isOneEditAwayCombined("pale", "ple"));
+        Assert.assertTrue(oneWay.isOneEditAwayCombined("pales", "pale"));
+        Assert.assertFalse(oneWay.isOneEditAwayCombined("palesx", "pale"));
+        Assert.assertTrue(oneWay.isOneEditAwayCombined("bale", "pale"));
+        Assert.assertFalse(oneWay.isOneEditAwayCombined("pale", "bae"));
+        Assert.assertFalse(oneWay.isOneEditAwayCombined("geeks", "geeks"));
+        Assert.assertFalse(oneWay.isOneEditAwayCombined("peaks", "geeks"));
     }
 }
